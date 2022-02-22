9850a0ac26753c75c32977deb0bc1ac902c87614
Jash Merchant <merchantjash@gmail.com>
2022-02-22 12:36:37 -0500

    Follow instructions

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
-web: gunicorn poll.wsgi --log-file -
-python manage.py collectstatic --noinput
-manage.py migrate
+web: gunicorn poll.wsgi
--- poll/settings.py
+++ poll/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import django_heroku
 
 from pathlib import Path
 
@@ -131,3 +132,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
