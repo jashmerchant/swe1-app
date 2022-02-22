b520ebaacd89ee9b5f66c28b04175cbf1b7c0ed9
Jash Merchant <merchantjash@gmail.com>
2022-02-22 14:18:49 -0500

    Add db con

--- poll/settings.py
+++ poll/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import dj_database_url
 import os
 import django_heroku
 
@@ -84,6 +85,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
